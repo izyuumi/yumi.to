a1f8ebbc1bee8e48be0b563b3ca21986329e28c9
Yumi Izumi <mail@yumi.to>
2023-10-21 19:34:09 -0400

    feat(robots): hide site from crawlers

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/supabase", "@nuxtjs/tailwindcss", "nuxt-headlessui"],
+  modules: [
+    "@nuxtjs/supabase",
+    "@nuxtjs/tailwindcss",
+    "nuxt-headlessui",
+    "@nuxtjs/robots",
+  ],
   supabase: {
     redirect: false,
+  },
+  robots: {
+    UserAgent: "*",
+    Disallow: "/",
   },
 });
