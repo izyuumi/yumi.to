eb77500859b5fe50ff4e5bafc6212c93bc538306
Yumi Izumi <mail@yumi.to>
2023-10-25 13:21:44 -0400

    feat(routes): add gh shortlink directory

---
+++ pages/gh/[short].vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+import z from "zod";
+import { useRoute } from "vue-router";
+
+const { params } = useRoute();
+const shortIsString = z.string().safeParse(params.short);
+if (!shortIsString.success) {
+	throw new Error("Shortlink is not a string");
+}
+const short = shortIsString.data;
+
+useExternalRedirect(`https://github.com/izyumidev/${short}`);
+</script>
+
+<template lang="pug">
+div.flex.justify-center.items-center.h-screen.w-screen.bg-black.text-white.text-2xl
+	p Redirecting...
+</template>
