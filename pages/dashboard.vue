a266f454cdc831cda9a5859199174610cbf3b575
Yumi Izumi <mail@yumi.to>
2024-09-09 10:53:03 -0400

    fix: correct Authenticate spelling

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,27 +1,27 @@
 <script lang="ts" setup>
-const user = useSupabaseUser()
+const user = useSupabaseUser();
 </script>
 
 <script lang="ts">
 const authenticate = async () => {
-  const supabase = useSupabaseClient()
+  const supabase = useSupabaseClient();
   const { error } = await supabase.auth.signInWithOAuth({
-    provider: 'github',
+    provider: "github",
     options: {
-      redirectTo: 'https://yumi.to/dashboard'
-    }
-  })
+      redirectTo: "https://yumi.to/dashboard",
+    },
+  });
   if (error) {
-    console.error(error)
+    console.error(error);
   }
-}
+};
 </script>
 
 <template lang="pug">
 ShortlinkList(v-if="user")
 div.flex.flex-col.items-center.justify-center.h-screen(v-if="!user")
-  button(@click="authenticate") Authenticte with GitHub
+  button(@click="authenticate") Authenticate with GitHub
 </template>
 <style lang="scss">
-@import '~/assets/dashboard.scss';
+@import "~/assets/dashboard.scss";
 </style>
